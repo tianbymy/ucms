ff4583e9a65077885ef69a18be25ba08d414aab5
Tianbymy <xiegang@zhiyisoft.com>
2013-08-29 17:01:42 +0800

    加入默认数据提示

--- app/models/ucms/type.rb
+++ app/models/ucms/type.rb
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 module Ucms
   class Type < ActiveRecord::Base
     attr_accessible :name, :code
     has_many :articles, class_name: "Ucms::Article"
+    # 文字、图片、下载
   end
 end
