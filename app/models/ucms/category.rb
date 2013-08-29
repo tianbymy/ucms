f9345d7d4e8549cb890bb436e1ae833c9cf347f2
Tianbymy <xiegang@zhiyisoft.com>
2013-08-29 12:49:29 +0800

    调整验证信息

--- app/models/ucms/category.rb
+++ app/models/ucms/category.rb
@@ -5,9 +5,8 @@ module Ucms
     attr_accessible :name,:parent_id
 
     validates_presence_of :name, :message => "不能为空"
-    validates_uniqueness_of  :name, :message => "不能重复"
     # 同一父级的组织名不能重复
-    validate { errors.add(:name, :taken) if siblings.reject{|x| x if x.id == self.id}.map(&:name).include?(self.name) }
+    validate { errors.add(:name, "同级别分类已经存在【#{name}】，请修改") if siblings.reject{|x| x if x.id == self.id}.map(&:name).include?(self.name) }
 
     has_many :articles,class_name: 'Ucms::Article'
 
